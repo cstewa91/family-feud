13c8d092cac1abf8ed1d22db3b4f390f8281cd60
unknown <cstewa91@gmail.com>
2019-09-02 00:47:52 -0700

    added reset board and added swtiching teams

--- main.js
+++ main.js
@@ -9,30 +9,50 @@ var answersAndQuestionsArray = [
         '2',
         'answer 3',
         '3'
+    ],
+    [
+        'question 2',
+        'answer 4',
+        '4',
+        'answer 5',
+        '5',
+        'answer 6',
+        '6'
     ]
 ]
+
+var team1Turn = true;
+var addedPoints;
+var answerShown = 0;
 
 function initApp() {
     addClickHandlers();
     addQuestionAndAnswer();
-    addAnswerPoints()
 }
 
 function addClickHandlers() {
     $('.answer').on('click', showAnswer)
-    $('.answer').on('click', addAnswerPoints)
 }
 
 function showAnswer() {
+    answerShown++;
     $(this).find('.number').css('display', 'none')
     $(this).find('.answer-text').css('display', 'flex')
     $(this).find('.answer-points').css('display', 'flex')
     $(this).addClass('no-click')
-    addTeamPoints()
+    addAnswerPoints($(this))
+    if(answerShown === 2) {
+        team1Turn();
+    }
+    if(checkAnswers()) {
+        addTeamPoints();
+    }
 }
 
 function addQuestionAndAnswer() {
-    var questionAndAnswer = answersAndQuestionsArray[0];
+    resetBoard();
+    var randomNumber = Math.floor(Math.random() * answersAndQuestionsArray.length);
+    var questionAndAnswer = answersAndQuestionsArray[randomNumber];
     var array = Object.values(questionAndAnswer);
     var index = 1; 
     for(var i = 0; i < array.length - 1; i+=2) {
@@ -43,21 +63,44 @@ function addQuestionAndAnswer() {
             $('.answer-' + index + ' .number').css('display', 'unset').text(index)
             index++;
     }
+    answersAndQuestionsArray.splice(randomNumber, 1)
 }
 
-function addAnswerPoints() {
-    if(!$(this).find('.answer-points').text()){
-        return;
-    }
+function addAnswerPoints(answer) {
     var totalPoints = parseInt($('.total-points').text());
-    var answerPoints = parseInt($(this).find('.answer-points').text());
-    var addedPoints = totalPoints + answerPoints;
+    var answerPoints = parseInt($(answer).find('.answer-points').text());
+    addedPoints = totalPoints + answerPoints;
     $('.total-points').text(addedPoints);
 }
 
-function addTeamPoints() {
-    var allAnswers = false;
+function checkAnswers() {
+    var allAnswers = true;
     $('.answer').each(function(){
-        
+        if(!$(this).hasClass('no-click')) {
+            allAnswers = false
+        }
     })
+    return allAnswers;
+}
+
+function addTeamPoints() {
+    if(team1Turn) {
+        $('#team-1-points').text(addedPoints)
+    } else {
+        $('#team-2-points').text(addedPoints)
+    }
+    setTimeout(function(){
+        addQuestionAndAnswer();
+    },1000)
+}
+
+function resetBoard() {
+    $('.total-points').text('0');
+    $('.number-container').css('display', 'flex')
+    $('.answer-text').css('display', 'none')
+    $('.answer-points').css('display', 'none')
+}
+
+function teamTurn() {
+    
 }
